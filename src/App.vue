9c02193f3d3bdcaa074abac8e0cf878f034c3e4c
kris299b <kripag01@easv365.dk>
2023-10-24 08:40:17 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,20 @@
 </nav>
 <RouterView />
 
+<div class="wrapper">
+      <header>
+        <i class="bx bx-cookie"></i>
+        <h2>Du bestemmer over dine data</h2>
+      </header>
+      <div class="data">
+        <p>Vi og vores samarbejdspartnere bruger teknologier, herunder cookies, til at indsamle oplysninger om dig <a href="#"> Læs mere..</a></p>
+      </div>
+      <div class="buttons">
+        <button class="button" id="acceptBtn">Accepter</button>
+        <button class="button" id="declineBtn">Afvis</button>
+      </div>
+</div>
+
 
 <footer class="bg-blue-400 dark:bg-blue-400">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
@@ -93,7 +107,6 @@ import { auth } from './firebase.js'
 import { signOut, onAuthStateChanged } from 'firebase/auth';
 import router from './router'
 
-
 let isLoggedIn = ref(false) 
 
 onMounted(() => {
@@ -121,8 +134,78 @@ let logOut = () => {
   })
 }
 
+
 </script>
 
 <style scoped>
+.wrapper {
+  position: fixed;
+  bottom: 50px;
+  right: -370px;
+  max-width: 345px;
+  width: 100%;
+  background:white;
+  padding: 15px 25px 22px;
+  transition: right 0.3s ease;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+}
+.wrapper.show {
+  right: 20px;
+}
+.wrapper header {
+  display: flex;
+  align-items: center;
+  column-gap: 15px;
+}
+header i {
+  color: #60a5fa; 
+  font-size: 32px;
+}
+header h2 {
+  color:#60a5fa; 
+  font-weight: 500;
+}
+.wrapper .data {
+  margin-top: 16px;
+}
+.wrapper .data p {
+  color: #333;
+  font-size: 16px;
+}
+.data p a {
+  color: #4070f4;
+  text-decoration: none;
+}
+.data p a:hover {
+  text-decoration: underline;
+}
+.wrapper .buttons {
+  margin-top: 16px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.buttons .button {
+  border: none;
+  color: #fff;
+  padding: 8px 0;
+  background:#60a5fa; 
+  cursor: pointer;
+  width: calc(100% / 2 - 10px);
+  transition: all 0.2s ease;
+}
+.buttons #acceptBtn:hover {
+  background-color: #60a5fa; 
+}
+#declineBtn {
+  border: 2px solid #60a5fa; 
+  background-color: #fff;
+  color: #60a5fa; 
+}
+#declineBtn:hover {
+  background-color: #4070f4;
+  color: #fff;
+}
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,25 @@
 <script setup>
 
+const cookieBox = document.querySelector(".wrapper"),
+  buttons = document.querySelectorAll(".button");
+const executeCodes = () => {
+  //if cookie contains codinglab it will be returned and below of this code will not run
+  if (document.cookie.includes("codinglab")) return;
+  cookieBox.classList.add("show");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+      //if button has acceptBtn id
+      if (button.id == "acceptBtn") {
+        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+        document.cookie = "cookieBy= codinglab; max-age=" + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+//executeCodes function will be called on webpage load
+window.addEventListener("load", executeCodes);
+
 </script>
 
 <template>
@@ -111,7 +131,6 @@
   height: 100%;
   top: 0;
   left: 0;
-  background: rgba(0, 0, 0, 0.208);
   z-index: 1;
 }
 
@@ -126,5 +145,6 @@
 #circle {
     border:4px solid #60a5fa;
 }
+
 
 </style>
